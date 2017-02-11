15332e7e39de06b9540ed6a312b8055b0883f3c7
Seth Woodworth <seth@sethish.com>
2017-02-11 14:36:30 -0500

    adds a quick and fast xml parser for python

---
+++ 04_romeo_xml_parse/parse_xml.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import xml.etree.ElementTree as ET
+
+import nltk
+
+
+def parse_xml():
+    tree = ET.parse('./romeo_and_juliet_moby.xml')
+    root = tree.getroot()
+    acts = root.findall('ACT')
+    romeo_data = []
+    for act in acts:
+        act_data = {'title': act.find('TITLE').text, 'scenes': []}
+        for scene in act.findall('SCENE'):
+            scene_data = {'title': scene.find('TITLE').text, 'speaches': []}
+            for speech in scene.findall('SPEECH'):
+                speaker = speech.find('SPEAKER').text
+                speech_data = {'speaker': speaker, 'lines': []}
+                for line in speech.findall('LINE'):
+                    speech_data['lines'].append(line.text)
+
+                scene_data['speaches'].append(speech_data)
+            act_data['scenes'].append(scene_data)
+        romeo_data.append(act_data)
+    return romeo_data
+
+if __name__ == "__main__":
+    print(parse_xml())
