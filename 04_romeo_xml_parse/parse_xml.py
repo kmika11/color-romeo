4e272593b0898bd9ef4a22f4ea22ce1fde7ae9df
Seth Woodworth <seth@sethish.com>
2017-02-11 16:52:19 -0500

    generate a different shape of data

--- 04_romeo_xml_parse/parse_xml.py
+++ 04_romeo_xml_parse/parse_xml.py
@@ -9,14 +9,10 @@ def parse_xml():
     tree = ET.parse('./romeo_and_juliet_moby.xml')
     root = tree.getroot()
     acts = root.findall('ACT')
-    romeo_data = []
+    romeo_array = []
     for act in acts:
-        act_data = {'title': act.find('TITLE').text, 'scenes': []}
         for scene in act.findall('SCENE'):
-            scene_data = {'title': scene.find('TITLE').text, 'speaches': []}
             for speech in scene.findall('SPEECH'):
-                speaker = speech.find('SPEAKER').text
-                speech_data = {'speaker': speaker, 'lines': []}
                 for line in speech.findall('LINE'):
                     line_text = ''
                     for ll in line.itertext():
@@ -24,19 +20,21 @@ def parse_xml():
 
                     if not line_text:
                         line_text = ''
-                    text_blob = TextBlob(line_text)
-                    speech_data['lines'].append([
-                        line_text,
-                        text_blob.sentiment.polarity,
-                        text_blob.sentiment.subjectivity
-                    ])
 
-                scene_data['speaches'].append(speech_data)
-            act_data['scenes'].append(scene_data)
-        romeo_data.append(act_data)
-    return romeo_data
+                    text_blob = TextBlob(line_text)
+                    romeo_array.append({
+                        'act': act.find('TITLE').text,
+                        'scene': scene.find('TITLE').text,
+                        'speaker': speech.find('SPEAKER').text,
+                        'line': line_text,
+                        'line_polarity': text_blob.sentiment.polarity,
+                        'line_subjectivity': text_blob.sentiment.subjectivity
+                    })
+
+    return romeo_array
 
 if __name__ == "__main__":
-    romeo_data = parse_xml()
-    with open('romeo_with_sentiment.json', 'w') as romeo_json:
-        json.dump(romeo_data, romeo_json, indent=4)
+    romeo_array = parse_xml()
+
+    with open('romeo_sentiment_array.json', 'w') as romeo_file:
+        json.dump(romeo_array, romeo_file, indent=4)
